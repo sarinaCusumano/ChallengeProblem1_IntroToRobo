c8adbc55537a23ca576c0143faf47984ef3738a0
Sarina <sarina.cusumano@gmail.com>
2025-09-16 21:04:30 -0600

    starting making bots based off n value and formatting stuff fixed

--- DroneNavigation.py
+++ DroneNavigation.py
@@ -1,25 +1,34 @@
 # Challenge Problem 1
 
-Class Robot:
-    def __init__(self, xPos, yPos, goalX, goalY, matrix, distance)
-        xPos = self.xPos
-        yPos = self.yPos
-        goalX = self.goalX
-        goalY = self.goalY
-        matrix = self.matrix
-        distance = self.distance
+import numpy as np
+import random as r
 
-    def get_xPos(self):
+class Robot:
+    def __init__(self, startX, startY, goalX, goalY, n_matrix):
+        self.startX = startX
+        self.startY = startY
+        self.goalX = goalX
+        self.goalY = goalY
+        self.n_matrix = n_matrix
+
+        self.matrix = np.zeros([self.n_matrix, self.n_matrix])
+        self.currentX = self.startX
+        self.currentY = self.startY
+    
+    def get_startX(self):
         return self.xPos
 
-    def set_xPos(self, x):
-        self.xPos = x
+    def set_currentX(self, x):
+        self.currentX = x
 
-    def get_yPos(self):
+    def get_startY(self):
         return self.yPos
 
-    def set_yPos(self, y):
-        self.yPos = y
+    def set_currentY(self, y):
+        self.currentY = y
+
+    def get_current_position(self):
+        return [self.currentY, self.currentX]
 
     def get_goalX(self):
         return self.goalX
@@ -33,32 +42,50 @@ Class Robot:
     def set_goalY(self, y):
         self.goalY = y
 
-    
+    def get_matrix(self):
+        return self.matrix
 
+    def update_matrix(self):
+        self.matrix[self.currentX, self.currentY] = 1
 
-Class Humanoid(Robot):
-    def __init__(self)
-
-Class Quadrotor(Robot):
-    def __init__(self)
-
-Class Drone(Robot):
-    def __init__(self):
-
-Class Grid:
-    def __init__(self):
-
-Class Node:
-    def __init__(self, state, occupied_bot, reserved_bot):
-        self.state = state # Open, occupied, reserved
-        self.occupied_bot = occupied_bot # The bot that is occupied at that node 
-        self.reserved_bot = reserved_bot # The bot that has the node reserved
+    def __str__(self):
+        return("This bot's position is [" + str(startX) + "," + str(startY) + "]")
         
-    def getBot(self):
-        return self.bot
 
-    def setBot(self, b):
-        self.bot = b
+
+class Humanoid(Robot):
+    def __init__(self, startX, startY, goalX, goalY, n_matrix):
+        super().__init__(startX, startY, goalX, goalY, n_matrix)
+
+class DifferentialDrive(Robot):
+    def __init__(self, startX, startY, goalX, goalY, n_matrix):
+        super().__init__(startX, startY, goalX, goalY, n_matrix)
+
+class Quadrotor(Robot):
+    def __init__(self, startX, startY, goalX, goalY, n_matrix):
+        super().__init__(startX, startY, goalX, goalY, n_matrix)
+
+class Grid:
+    def __init__(self, size):
+        self.size=size
+
+        self.grid = np.empty((size,size), Node)
+        for r in range(size):
+            for c in range(size):
+                self.grid[r,c] = Node(True)
+        
+
+# Creates a Node Class so that the attriutes of each node in the grid can be accessed
+class Node:
+    def __init__(self, state: True):
+        self.state = state # Open = True, Occupied = False
+        #self.bot = bot # Bot in that node 
+        
+##    def getBot(self):
+##        return self.bot
+##
+##    def setBot(self, b):
+##        self.bot = b
 
     def getState(self):
         return self.state
@@ -66,8 +93,38 @@ Class Node:
     def setState(self, s):
         self.state = s
 
-    
 
+if __name__ == "__main__":
+    
+    num = int(input("Please enter a number (between 3-10): "))
+
+    grid = Grid(num)
+
+    bots = []
+    
+    # Creates bots based on input value, randomly creates start and goal locations, adds them to Grid
+    for i in range(0,num*2):
+        bot = r.randint(1,3)
+        startX = r.randint(0,num)
+        startY = r.randint(0,num)
+        goalX = r.randint(0,num)
+        goalY = r.randint(0,num)
+        if bot == 1:
+            h = Humanoid(startX, startY, goalX, goalY, num)
+            bots.append(h)
+        elif bot ==2:
+            d = DifferentialDrive(startX, startY, goalX, goalY, num)
+            bots.append(d)
+        else:
+            q = Quadrotor(startX, startY, goalX, goalY, num)
+            bots.append(q)
+
+    for i in range(0, num*2):
+        print(str(bots[i]))
+
+
+        
+        
     
 
 
